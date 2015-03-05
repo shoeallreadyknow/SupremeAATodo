bb171f465846ca6cd6cc3c62fef5678aa8420b15
justin landaverde <justlandaverde28@gmail.com>
2015-03-05 14:45:52 -0800

    basic model of the design

--- app/src/main/java/android/supremeaa/todo/TodoActivity.java
+++ app/src/main/java/android/supremeaa/todo/TodoActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class TodoActivity extends Activity {
@@ -12,25 +14,27 @@ public class TodoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.todo, menu);
-        return true;
-    }
+        setContentView(R.layout.activity_todo);
+        String clean_room = "clean the room";
+        String mowe_lawn = "mowe lawn";
+        String go_to_store = "go to the store";
+        String wash_car = "wash the car";
+        String walk_dog = "walk the dog";
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+
+        String[] listItems = new String[5];
+        listItems[0] = clean_room;
+        listItems[1] = mowe_lawn;
+        listItems[2] = go_to_store;
+        listItems[3] = wash_car;
+        listItems[4] = walk_dog;
+
+
+        ArrayAdapter<String> listItemAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        ListView lv = (ListView) this.findViewById(R.id.listView);
+        lv.setAdapter(listItemAdapter);
+
     }
 }
